273f713d81b6ca760efa44e3d64ba3e096d33643
hugoapi <hugobaugnies2000@gmail.com>
2024-06-14 15:38:38 +0200

    Update api.py

--- api.py
+++ api.py
@@ -24,7 +24,7 @@ def get_file_sha():
     try:
         url = "https://api.github.com/repos/hugoapi/hugo_api/contents/database.csv"
         headers = {
-            'Authorization': 'token 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'  
+            'Authorization': 'token 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'  
         }
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -55,7 +55,7 @@ def update_github_file(filepath="database.csv", message="Update data.csv"):
             "sha": sha
         }
         headers = {
-            'Authorization': 'token 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' 
+            'Authorization': 'token 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' 
         }
         response = requests.put(url, json=payload, headers=headers)
         response.raise_for_status()
